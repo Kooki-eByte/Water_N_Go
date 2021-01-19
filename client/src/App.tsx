05c2d19d7ab614550c772bfb12b9404e8540a1f8
Cristian Hornedo <officialcristianl@gmail.com>
2021-01-19 18:04:24 -0500

    📖 Started on the home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Home } from './pages/Home';
 
 const App: React.FC = () => {
   return (
     <div>
-      Water "N" Go!
+      <Router>
+        <Route exact path="/" component={Home} />
+      </Router>
     </div>
   );
 }
